a2d9d9087e8e9110a149e505d63fe7f687319727
Dusan Trickovic <dusan.trickovic@akvelon.com>
2023-03-11 17:06:21 +0100

    Add logic for fetching PR information about the repository

--- index.js
+++ index.js
@@ -4,57 +4,74 @@ const axios = require('axios');
 const readline = require('readline');
 require('dotenv').config();
 
-const API_ENDPOINT_URL = 'https://api.github.com/search/issues';
-
 try {
     const dateObject = new Date();
     const CURRENT_DATE = `${dateObject.getFullYear()}-${dateObject.getMonth() + 1}-${dateObject.getDate()}`;
+
     let CUSTOM_DATE = core.getInput('custom-date') === '' ? CURRENT_DATE : core.getInput('custom-date');
     const REPO_NAME = core.getInput('repository-name');
     const GITHUB_TOKEN = core.getInput('repo-token');
 
-    async function fetchIssues(repositoryName, state = 'all', date = null) {
+    async function fetchData(repositoryName, states = [], date = null, type='issue') {
         try {
-            const query = `q=repo:actions/${repositoryName}+type:issue`
+            const API_ENDPOINT_URL = 'https://api.github.com/search/issues';
+            const query = `q=repo:actions/${repositoryName}+type:${type.toLowerCase()}`
             const urlWithRepoName = `${API_ENDPOINT_URL}?${query}`
 
-            if (date !== null && date !== CURRENT_DATE) {
-                const urlWithQueryWithDate = `${urlWithRepoName}+created:>${date}`
-                const { data } = await axios({
+            for(let i = 0; i < states.length; i++) {
+                
+                if (date !== null && date !== CURRENT_DATE) {
+                    const urlWithQueryWithDate = `${urlWithRepoName}+created:>${date}`
+                    await axios({
+                        method: 'get',
+                        url: `${urlWithQueryWithDate}${states[i] === 'all' ? '&state:all' : `+state:${states[i]}`}`,
+                        headers: {
+                            authentication: `token ${GITHUB_TOKEN}`
+                        }
+                    })
+                    .then(({data}) => {
+                        console.log(`Number of ${states[i]} ${type}s after ${date}: ${data.total_count}`);
+                    });
+                    continue;
+                }
+
+                await axios({
                     method: 'get',
-                    url: `${urlWithQueryWithDate}${state === 'all' ? '&state:all' : `+state:${state}`}`,
+                    url: `${urlWithRepoName}${states[i] === 'all' ? '&state:all' : ` state:${states[i]}`}`,
                     headers: {
                         authentication: `token ${GITHUB_TOKEN}`
                     }
+                })
+                .then(({data}) => {
+                    console.log(`Total number of ${states[i]} ${type}s: ${data.total_count}`);
                 });
-                console.log(`Number of ${state} issues after ${date}: ${data.total_count}`);
-                return data.total_count;
+                continue;
             }
 
-            const { data } = await axios({
-                method: 'get',
-                url: `${urlWithRepoName}${state === 'all' ? '&state:all' : ` state:${state}`}`,
-                headers: {
-                    authentication: `token ${GITHUB_TOKEN}`
-                }
-            });
-            console.log(`Number of ${state} issues: ${data.total_count}`);
-            return data.total_count;
         } catch (error) {
             console.error(error);
             throw new Error('Error fetching data');
         }
     }
     
-    async function showTotalIssues(repositoryName, date = undefined) {
-        await fetchIssues(repositoryName, 'all', date);
-        await fetchIssues(repositoryName, 'open', date);
-        await fetchIssues(repositoryName, 'closed', date);
+    
+    async function showAllData(repositoryName, date = undefined) {
+        console.log('--------------------------');
+        console.info(`REPOSITORY NAME: actions/${REPO_NAME}`);
+        console.log('--------------------------');
+
+        await fetchData(repositoryName, ['all', 'open', 'closed'], date, 'issue');
+
+        console.log('--------------------------');
+
+        await fetchData(repositoryName, ['all', 'open', 'closed'], date, 'PR');
+
+        console.log('--------------------------');
     }
 
     async function main() {
-        const stringWithNotNullDate = 'Date provided. Gathering Issue information since the date provided'
-        const stringWithNullDate = 'Date not provided. Gathering Issue information in total numbers up to now...'
+        const stringWithNotNullDate = `Date provided. Gathering information for 'actions/${REPO_NAME}' since the date provided`
+        const stringWithNullDate = `Date not provided. Gathering information for 'actions/${REPO_NAME}' in total numbers up to now...`
 
         if (CUSTOM_DATE === '' || CUSTOM_DATE === CURRENT_DATE) {
             console.log(stringWithNullDate);
@@ -63,7 +80,7 @@ try {
             console.log(`${stringWithNotNullDate} (${CUSTOM_DATE})`);
         }
 
-        showTotalIssues(REPO_NAME, CUSTOM_DATE);
+        showAllData(REPO_NAME, CUSTOM_DATE);
 
     };
 
