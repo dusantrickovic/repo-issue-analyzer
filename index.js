002a3c17f07d7cce13ef855837fa3cbc69c31931
Dusan Trickovic <dusan.trickovic@akvelon.com>
2023-03-11 09:55:09 +0100

    First attempt at creating the Action

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ require('dotenv').config();
 const API_ENDPOINT_URL = 'https://api.github.com/search/issues';
 
 try {
-    const repositoryName = core.getInput('repository-name');
-    let startingTime = core.getInput('custom-date');
-    if (!startingTime) {
-        startingTime = new Date().toTimeString();
+    const REPO_NAME = core.getInput('repository-name');
+    const CUSTOM_DATE = core.getInput('custom-date');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    if (!CUSTOM_DATE) {
+        CUSTOM_DATE = new Date().toTimeString();
     }
 
     async function fetchIssues(repositoryName, state = 'all', date = null) {
         try {
-            const query = `q=repo:actions/${repositoryName}+type:issue`
+            const query = `q=repo:actions/${REPO_NAME}+type:issue`
             const urlWithRepoName = `${API_ENDPOINT_URL}?${query}`
 
             if (date !== null) {
@@ -24,7 +25,7 @@ try {
                     method: 'get',
                     url: `${urlWithQueryWithDate}${state === 'all' ? '&state:all' : `+state:${state}`}`,
                     headers: {
-                        authentication: `token ${process.env.GITHUB_ACCESS_TOKEN}`
+                        authentication: `token ${GITHUB_TOKEN}`
                     }
                 });
                 console.log(`Number of ${state} issues after ${date}: ${data.total_count}`);
@@ -35,7 +36,7 @@ try {
                 method: 'get',
                 url: `${urlWithRepoName}${state === 'all' ? '&state:all' : ` state:${state}`}`,
                 headers: {
-                    authentication: `token ${process.env.GITHUB_ACCESS_TOKEN}`
+                    authentication: `token ${GITHUB_TOKEN}`
                 }
             });
             console.log(`Number of ${state} issues: ${data.total_count}`);
@@ -52,28 +53,29 @@ try {
         await fetchIssues(repositoryName, 'closed', date);
     }
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
+    // const rl = readline.createInterface({
+    //     input: process.stdin,
+    //     output: process.stdout,
+    // });
 
     async function main() {
-        rl.question('Please provide the name of the Actions repository you wish to check: ', (repositoryName) => {
-            rl.question('Do you want to see the issues generated on a custom date? (yes/no) ', (answer) => {
-                finalAnswer = answer;
-                if (answer.toLowerCase() === 'yes') {
-                    rl.question('Please input a date [YYYY-MM-DD]: ', (date) => {
-                    console.log(`You entered the date: ${date}. Showing the numbers...`);
-                    showTotalIssues(repositoryName, date)
-                    rl.close();
-                });
-                } else {
-                    console.log('Okay, showing total numbers instead...');
-                    showTotalIssues(repositoryName, null);
-                    rl.close();
-                }
-            });
-        });
+
+        console.log('Displaying issue information based on your input...');
+        showTotalIssues(REPO_NAME, CUSTOM_DATE);
+
+        // rl.question('Do you want to see the issues generated on a custom date? (yes/no) ', (answer) => {
+        //         if (answer.toLowerCase() === 'yes') {
+        //             rl.question('Please input a date [YYYY-MM-DD]: ', (date) => {
+        //             console.log(`You entered the date: ${date}. Showing the numbers...`);
+        //             showTotalIssues(REPO_NAME, date)
+        //             rl.close();
+        //         });
+        //         } else {
+        //             console.log('Okay, showing total numbers instead...');
+        //             showTotalIssues(REPO_NAME, null);
+        //             rl.close();
+        //         }
+        //     });
     };
 
     main();
