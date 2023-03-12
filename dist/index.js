e9eb87a6e93905f3d794f876206bf5db6a754afd
Dusan Trickovic <dusan.trickovic@akvelon.com>
2023-03-12 11:18:47 +0100

    Fix the bug that prevents from searching for dates in the future

--- dist/index.js
+++ dist/index.js
@@ -17012,6 +17012,9 @@ try {
     function isValidDateFormat(date) {
         // Use a regular expression to match the date format
         const dateFormatString = /^\d{4}-\d{2}-\d{2}$/;
+        // Find current date
+        const currentDay = dateObject.getDate();
+        const currentMonth = dateObject.getMonth() + 1;  // Begins counting from 0
         const currentYear = dateObject.getFullYear();
       
         // If the date string matches the regular expression, check if the month and day values are valid
@@ -17020,7 +17023,8 @@ try {
           const month = parseInt(date.slice(5, 7));
           const day = parseInt(date.slice(8, 10));
       
-          if (month <= 12 && day <= 31 && year <= currentYear) {
+          // There's no month past December and no day past 31st and no search can look into the future
+          if ((month <= 12 && month <= currentMonth) && (day <= 31 && day <= currentDay) && year <= currentYear) {
             return true;
           }
         }
